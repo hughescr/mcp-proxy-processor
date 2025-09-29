3a6da79853aa6b61458ecb8b4c3522a7291b1047
Craig R. Hughes <craig.git@rungie.com>
2025-09-29 15:34:50 -0700

    Better eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import defaultConfig from '@hughescr/eslint-config-default';
 import packageJson from 'eslint-plugin-package-json';
+
 import tseslint from 'typescript-eslint';
 
 export default
 [
     {
-        name: 'ignores',
-        ignores: ['coverage', 'node_modules', '*.md'],
+        name: 'local-project-ignores',
+        ignores: ['coverage', '*.md'],
     },
 
     defaultConfig.configs.recommended,
@@ -15,10 +16,13 @@ export default
     ...tseslint.configs.stylistic,
 
     {
+        name: 'TypeScript overrides',
         rules: {
+            '@typescript-eslint/triple-slash-reference': 'off',
             '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
             '@stylistic/operator-linebreak': 'off',
             'n/no-missing-import': 'off',
+            'n/no-sync': 'off',
         },
     },
 
@@ -31,7 +35,7 @@ export default
     },
 
     {
-        files: ['**/*.js', '**/*.mjs'],
+        files: ['**/*.js', '**/*.mjs', ...packageJson.configs.recommended.files],
         ...tseslint.configs.disableTypeChecked,
     },
 ];
